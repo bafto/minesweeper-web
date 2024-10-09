8bb35b2794a614be08b90a6223abd71984016ddb
NotLe0n <notle0n75@gmail.com>
2024-10-09 12:53:45 +0200

    images as buttons, moved click event to html

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,13 +1,9 @@
-document.querySelectorAll('.cell').forEach(cell => {
-	if (cell.innerText == '#') {
-		cell.addEventListener('click', _ => {
-			console.log('click');
-			fetch(`/reveal?x=${cell.getAttribute('x')}&y=${cell.getAttribute('y')}`).then(() => {
-				window.location.reload()
-			})
-		})
-	}
-});
+function reveal(cell) {
+	console.log('click');
+	fetch(`/reveal?x=${cell.getAttribute('x')}&y=${cell.getAttribute('y')}`).then(() => {
+		window.location.reload()
+	})
+}
 
 function retry() {
 	fetch('/retry').then(() => {
