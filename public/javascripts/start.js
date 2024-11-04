d43bcd87e992d676ba1ea0ffde821b7f47df3043
NotLe0n <notle0n75@gmail.com>
2024-11-04 11:15:44 +0100

    validate input

--- public/javascripts/start.js
+++ public/javascripts/start.js
@@ -1,8 +1,12 @@
 async function startGame() {
-	const width = document.getElementById('input-width').value;
-	const height = document.getElementById('input-height').value;
-	const bomb_chance = document.getElementById('input-bomb_chance').value;
-	const max_undos = document.getElementById('input-max_undos').value;
+	const width = document.getElementById('input-width');
+	const height = document.getElementById('input-height');
+	const bomb_chance = document.getElementById('input-bomb_chance');
+	const max_undos = document.getElementById('input-max_undos');
+
+	if (validate(width) | validate(height) | validate(max_undos)) {
+		return;
+	}
 
 	fetch('/api/start_game', {
 		method: 'POST',
@@ -11,12 +15,23 @@ async function startGame() {
 			'Csrf-Token': getCookieByName('play-csrf-token'),
 		},
 		body: JSON.stringify({
-			width: Number.parseInt(width),
-			height: Number.parseInt(height),
-			bomb_chance: Number.parseFloat(bomb_chance),
-			max_undos: Number.parseInt(max_undos)
+			width: Number.parseInt(width.value),
+			height: Number.parseInt(height.value),
+			bomb_chance: Number.parseFloat(bomb_chance.value),
+			max_undos: Number.parseInt(max_undos.value)
 		})
 	}).then(reload_page).catch(console.error);
+}
+
+function validate(elm) {
+	if (elm.value !== "") {
+		elm.classList.remove("invalid");
+		return false;
+	}
+
+	elm.classList.add("invalid");
+
+	return true;
 }
 
 function getCookieByName(name) {
